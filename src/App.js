4bb46653fd9f9bc59794cccf998f631cfc9db027
wjarocinski <wojciech.jarocinski@atos.net>
2019-05-30 15:59:09 +0200

    sign up, sign in, dashboard, projectDetails components added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
+import ProjectDetails from "./components/project/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
